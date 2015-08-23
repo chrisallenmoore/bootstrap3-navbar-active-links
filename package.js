c71cfedc61263d2299ffb107e8ab13fa61b92e2f
Chris Allen Moore <chris@chrisallenmoore.com>
2015-08-23 21:48:00 +0700

    added some dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cmoore:bootstrap3-navbar-active-links',
-  version: '0.0.1',
+  version: '0.0.6',
   // Brief, one-line summary of the package.
   summary: 'Adds an ".active" class to links in a bootstrap3 navbar that point to current path',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.addFiles('bootstrap3-navbar-active-links.js');
+  api.use([
+    'aldeed:template-extension@3.4.3',
+    'iron:router@1.0.9',
+    'templating@1.1.1'
+  ], 'client');
+  api.addFiles('bootstrap3-navbar-active-links.js', 'client');
 });
 
 Package.onTest(function(api) {
